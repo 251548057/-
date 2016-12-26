a108759bb83fab329c02a4a4418c22a42884d509
unknown <you@example.com>
2016-12-26 09:46:32 +0800

    gaoyin

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('nodemon', function(ab){
 gulp.task('browserSync', ['nodemon'], function(){
     browserSync.init({
         proxy: {
-            target: 'http://127.0.0.1:9978'
+            target: 'http://127.0.0.1:16921'
         },
         files: ['*'],
         port: 9888,
--- server.js
+++ server.js
@@ -9,12 +9,20 @@ var proxy = require('http-proxy-middleware');
 var app = express();
 
 //定义通过 /api 访问的请求，转发到指定路径
-app.use('/api', proxy({
-    target: 'http://122.10.30.153:9901',
-    pathRewrite: {
-        '^/api': '/'
+app.use('/api',proxy({
+    target:'http://guanjp.com:9805',
+    changeOrigin:true,
+    ws:true,
+    cookieRewrite:true,
+    pathRewrite:{
+        '^/api':'/'
     }
 }));
+// http://127.0.0.1:9888/api/index
+// 替换为http://122.10.30.153:9901/index
+
+console.log(__dirname);
+console.log(__filename);
 //   http://127.0.0.1:9888/api/index
 // 替换为  http://122.10.30.153:9901/index
 
@@ -35,6 +43,31 @@ app.use('/public', express.static(publicPath));
 // 定义一个接口
 app.get('/login', function(req, res){
     res.send('ha ha ha');
+});
+
+var sha1 = require("sha1");
+//定义验证接口
+app.use('/weixin', function(req, res){
+    //获取get传递数据
+    var obj = req.query;
+    console.log("weixin", obj);
+    //将数据添加到一个数组
+    var arr = ["gaoyin", obj.timestamp, obj.nonce];
+    //排序
+    arr.sort();
+
+    //拼接字符串，并进行 sha1 加密
+    var str = sha1(arr.join(""));
+    console.log('sha1   ', str);
+
+    console.log('signature', obj.signature === str);
+    //匹配是否是微信请求
+    if( obj.signature === str){
+        //成功返回 echostr 随机字符串
+        res.send(obj.echostr).end();
+    }else{
+        res.send("验证失败").end();
+    }
 });
 
 
